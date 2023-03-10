f7df04866402994036ad0bcf21a5896f8dc57412
UnAfraid <unafraid89@gmail.com>
2023-03-10 23:36:07 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -68,39 +68,37 @@ async fn get_adapter() -> anyhow::Result<Adapter> {
 }
 
 fn process_central_event(event: CentralEvent) -> anyhow::Result<Vec<u8>> {
-    let payload: Vec<u8>;
     match event {
         CentralEvent::DeviceDiscovered(id) => {
-            payload = serde_json::to_vec(&DeviceDiscoveredEvent { event: "DeviceDiscovered".into(), id })?;
+            Ok(serde_json::to_vec(&DeviceDiscoveredEvent { event: "DeviceDiscovered".into(), id })?)
         }
         CentralEvent::DeviceUpdated(id) => {
-            payload = serde_json::to_vec(&DeviceUpdatedEvent { event: "DeviceUpdated".into(), id })?;
+            Ok(serde_json::to_vec(&DeviceUpdatedEvent { event: "DeviceUpdated".into(), id })?)
         }
         CentralEvent::DeviceConnected(id) => {
-            payload = serde_json::to_vec(&DeviceConnectedEvent { event: "DeviceConnected".into(), id })?;
+            Ok(serde_json::to_vec(&DeviceConnectedEvent { event: "DeviceConnected".into(), id })?)
         }
         CentralEvent::DeviceDisconnected(id) => {
-            payload = serde_json::to_vec(&DeviceDisconnectedEvent { event: "DeviceDisconnected".into(), id })?;
+            Ok(serde_json::to_vec(&DeviceDisconnectedEvent { event: "DeviceDisconnected".into(), id })?)
         }
         CentralEvent::ManufacturerDataAdvertisement { id, manufacturer_data } => {
             let data = manufacturer_data.
                 iter().
                 map(|(k, v)| (k.clone(), hex::encode(v))).
                 collect();
-            payload = serde_json::to_vec(&ManufacturerDataAdvertisementEvent { event: "ManufacturerDataAdvertisement".into(), id, manufacturer_data: data })?;
+            Ok(serde_json::to_vec(&ManufacturerDataAdvertisementEvent { event: "ManufacturerDataAdvertisement".into(), id, manufacturer_data: data })?)
         }
         CentralEvent::ServiceDataAdvertisement { id, service_data } => {
             let data = service_data.
                 iter().
                 map(|(k, v)| (k.clone(), hex::encode(v))).
                 collect();
-            payload = serde_json::to_vec(&ServiceDataAdvertisementEvent { event: "ServiceDataAdvertisement".into(), id, service_data: data })?;
+            Ok(serde_json::to_vec(&ServiceDataAdvertisementEvent { event: "ServiceDataAdvertisement".into(), id, service_data: data })?)
         }
         CentralEvent::ServicesAdvertisement { id, services } => {
-            payload = serde_json::to_vec(&ServicesAdvertisementEvent { event: "ServicesAdvertisement".into(), id, services })?;
+            Ok(serde_json::to_vec(&ServicesAdvertisementEvent { event: "ServicesAdvertisement".into(), id, services })?)
         }
     }
-    Ok(payload)
 }
 
 async fn mqtt_init(config: &Config) -> anyhow::Result<AsyncClient> {
@@ -130,30 +128,30 @@ async fn mqtt_init(config: &Config) -> anyhow::Result<AsyncClient> {
 
 async fn publish_to_topic<'a>(mqtt_client: &AsyncClient, topic: &Topic<'a>, payload: &Vec<u8>) {
     for _ in 1..=30 {
-        let mut reconnected = false;
         match topic.publish(payload.to_owned()).await {
             Ok(_) => { return; }
             Err(err) => match err {
                 PahoDescr(id, reason) => {
                     if id != MQTT_CLIENT_DISCONNECTED {
                         error!("Failed to publish message id: {:?} reason: {:?}", id, reason);
+                        time::sleep(Duration::from_secs(1)).await;
                         continue;
                     }
 
                     warn!("Connection to the mqtt broker was lost, attempting to reconnect..");
                     match mqtt_client.reconnect().await {
-                        Ok(_) => {
-                            info!("Reconnected");
-                            reconnected = true;
+                        Ok(_) => info!("Reconnected"),
+                        Err(reconnect_err) => {
+                            error!("Failed to reconnect: {:?}", reconnect_err);
+                            time::sleep(Duration::from_secs(1)).await;
                         }
-                        Err(reconnect_err) => error!("Failed to reconnect: {:?}", reconnect_err),
                     }
                 }
-                _ => error!("Failed to publish message: {:?}", err),
+                _ => {
+                    error!("Failed to publish message: {:?}", err);
+                    time::sleep(Duration::from_secs(1)).await;
+                }
             }
-        }
-        if !reconnected {
-            time::sleep(Duration::from_secs(1)).await;
         }
     }
 }
